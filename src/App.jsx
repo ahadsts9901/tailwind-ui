fdc93fa0ad46f2948d53d9b3610b839d448c1070
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2024-02-09 12:00:59 +0500

    code

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import "./tailwind.css"
+import './App.css'
+import HeroSection from "./TailwindUI/HeroSection"
+import FeatureSection from "./TailwindUI/FeatureSection"
+import Header from "./TailwindUI/Header"
+import NotFound from "./TailwindUI/NotFound"
+import StackedLayout from "./TailwindUI/StackedLayout"
+import List from "./TailwindUI/List"
+import DialogBox from "./TailwindUI/DialogBox"
+import DropDown from "./TailwindUI/DropDown"
+import ProductDetails from "./TailwindUI/ProductDetails"
+import ShoppingCart from "./TailwindUI/ShoppingCart"
+import ProductList from "./TailwindUI/ProductList"
+
+function App() {
+
+  // tailwind ui is paid but these are some free components & pages
+
+  return (
+    <>
+    {/* <HeroSection/> */}
+    {/* <FeatureSection/> */}
+    {/* <Header/> */}
+    {/* <NotFound/> */}
+    {/* <StackedLayout/> */}
+    {/* <List/> */}
+    {/* <DialogBox/> */}
+    {/* <DropDown/> */}
+    {/* <ProductDetails/> */}
+    {/* <ShoppingCart/> */}
+    {/* <ProductList/> */}
+    </>
+  )
+}
+
+export default App
